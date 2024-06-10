1272d78ebd25f13fe0745c7f2797ecf298330389
Youssef Makboul <youssef.makboul@shopify.com>
2024-06-10 10:08:56 -0400

    Add neotest stuff

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -57,7 +57,10 @@ return {
             "nvim-neotest/nvim-nio",
             "nvim-lua/plenary.nvim",
             "antoinemadec/FixCursorHold.nvim",
-            "nvim-treesitter/nvim-treesitter"
+            "nvim-treesitter/nvim-treesitter",
+            "olimorris/neotest-rspec",
+            "rouge8/neotest-rust",
+            "zidhuss/neotest-minitest",
         }
     },
     { 'ruanyl/vim-gh-line' },
@@ -68,6 +71,33 @@ return {
             "nvim-lua/plenary.nvim",
             "antoinemadec/FixCursorHold.nvim",
             "nvim-treesitter/nvim-treesitter"
-        }
+        },
+        keys = {
+            {
+                "<leader>tt",
+                function()
+                    require("neotest").run.run()
+                end,
+                desc = "Run closest test",
+            },
+            {
+                "<leader>tf",
+                function()
+                    require("neotest").run.run(vim.fn.expand("%"))
+                end,
+                desc = "Run file",
+            },
+        },
+        config = function()
+            ---@diagnostic disable-next-line: missing-fields
+            require("neotest").setup({
+                adapters = {
+                    require("neotest-rspec"),
+                    require("neotest-rust"),
+                    require("neotest-minitest"),
+                },
+            })
+        end,
+
     },
 }

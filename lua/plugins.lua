f055a92704ce8ed344574f642cf40e0223257c3f
Youssef Makboul <youssef.makboul@shopify.com>
2024-10-30 11:45:58 -0400

    add ripgrep

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -4,9 +4,11 @@ return {
     { "folke/neoconf.nvim",   cmd = "Neoconf" },
 
     'nvim-telescope/telescope.nvim',
-    tag = '0.1.6',
-    -- or                              , branch = '0.1.x',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    tag = '0.1.x',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        'BurntSushi/ripgrep',
+    },
     { 'nvim-lua/plenary.nvim' },
 
     {
